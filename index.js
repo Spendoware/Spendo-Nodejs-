bf44163c77bb6b0a94eec97fdcd1058264657888
Realhaqq <haqqq4peace@gmail.com>
2022-08-10 14:36:43 +0100

    published correct one

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
 const axios = require('axios')
-const shortid = require('shortid')
 const serviceTypes = require("./constants/services");
-const config = require('config.json');
-
 const baseUrl = "https://gateway.spendoware.com";
 
 
-function Spendo(param) {
+/**
+ * @description initialize the Spendoware wrapper function
+ * @param {object} param =>  apiKey, data, serviceType
+ * @return {function} request function
+ */
+async function Spendo(param) {
     if (!param) return console.log('Error: apiKey, are required!')
 
     const { apiKey, data, serviceType } = param
-
-     /**
+    
+    /**
    * makes an encrypted call to Spendo API
-   * @param {object} params => serviceType, apiKey, data
+   * @param {object} params => apiKey, data, serviceType 
    * @param {function} callback gets called with the result(data) object
-   * @return {object} data return data response object
+   * @return {object} data return decrypted data response object
    */
     
-      async function request (params, callback) {
-          if (!params) return console.log('Error: serviceType, data are required!')
+    if (!apiKey) return console.log('Error: apikey, data are required!')
+    if (!serviceType) return console.log('Error: serviceType are required!')
+    if (!data) return console.log('Error: data are required!')
           
-    
-        var payload = data
-        
+    var axiosConfig = {
+        method: 'POST',
+        url: `${baseUrl}/gateway/api/v1/${serviceType}`,
+        headers: {
+            'Content-Type': 'application/json',
+            'apiKey': apiKey
+        },
+        data: data
+    };
           
-        const { data: Response } = await axios.post(`${baseUrl}/gateway/api/v1/${serviceType}`, {
-            data: payload
-          }, {
-            headers: {
-                'Content-Type': 'application/json',
-                'apiKey': apiKey
+    const response = axios(axiosConfig)
+        .then(function (response) {
+            if (response.status === 200) {
+                return JSON.stringify(response.data)
             }
         })
+        .catch(function (error) {
+            console.log(error);
+        });
           
-          if (callback) {
-        
-              callback(Response)
-          }
-          
-          return Response
-        }
-          
-          
-          return request;
-
+    return await response??console.log('Error: no data returned!')
 }
 
 
